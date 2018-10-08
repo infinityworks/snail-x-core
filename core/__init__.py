02ce48b99182044944919c8a9b9972770c4a24d8
Emily <emily.birch@infinityworks.com>
2018-10-08 15:12:25 +0100

    Flask-SQLAlchemy functionality commented out while testing Heroku setup

--- core/__init__.py
+++ core/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'random'
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 toolbar = DebugToolbarExtension(app)
