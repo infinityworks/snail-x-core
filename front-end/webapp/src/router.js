4d9ae9e3ff15729566f91b17c0d0eeebf3d9f59f
Mike Winker <mikewinker@gmail.com>
2018-10-09 09:48:16 +0100

    Moved auth components to sub directory

--- front-end/webapp/src/router.js
+++ front-end/webapp/src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import RegisterComponent from "./components/Register.vue"
+import RegisterComponent from "./components/auth/Register.vue"
 import HomeComponent from "./components/Home.vue"
-import LoginComponent from "./components/Login.vue"
+import LoginComponent from "./components/auth/Login.vue"
 
 Vue.use(Router);
 
