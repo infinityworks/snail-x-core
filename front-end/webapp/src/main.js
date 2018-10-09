69e9fcc49dd4020a5bf252691fdbe867b66abb46
Mike Winker <mikewinker@gmail.com>
2018-10-09 17:18:06 +0100

    Commented out rerouting by visitor type

--- front-end/webapp/src/main.js
+++ front-end/webapp/src/main.js
@@ -6,25 +6,24 @@ import VueResource from "vue-resource"
 import Vuex from 'vuex'
 import 'es6-promise/auto'
 
-
 Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 Vue.use(VueResource);
 
-router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresVisitor)) {
-        if (this.$store.getters.loggedIn) {
-            next({
-                name: 'home',
-            })
-        } else {
-            next()
-        }
-    } else {
-        next()
-    }
-});
+// router.beforeEach((to, from, next) => {
+//     if (to.matched.some(record => record.meta.requiresVisitor)) {
+//         if (store.getters.loggedIn) {
+//             next({
+//                 name: 'home',
+//             })
+//         } else {
+//             next()
+//         }
+//     } else {
+//         next()
+//     }
+// });
 
 new Vue({
     router,
