56e20a57236cd12dfa69ebd0fdf1f762a530b386
JamesSheard <james.sheard@infinityworks.com>
2018-10-10 16:48:22 +0100

    Password Encrypting

--- core/source/user_source.py
+++ core/source/user_source.py
@@ -1,13 +1,18 @@
 from core.db.db_func import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 
+def hash_password(password):
+    return generate_password_hash(password)
 
 def set_new_user(user):
     db = get_db()
     cursor = db.cursor()
 
+    hashed_password = generate_password_hash(user.password)
+
     sqlstatement = "INSERT INTO users (firstName, lastName, email, password) VALUES ('{}', '{}', '{}', '{}');"\
         .format(
-            user.first_name, user.last_name, user.email, user.password
+            user.first_name, user.last_name, user.email, hashed_password
         )
 
     try:
--- core/tests/test_user_repository.py
+++ core/tests/test_user_repository.py
@@ -1,17 +1,11 @@
-# import unittest
-# from core.repositories.user_repository import UserRepository
+import unittest
+from core.source.user_source import hash_password
+import psycopg2
 #
-# class TestUserRepo(unittest.TestCase):
-#
-#
-#     def test_register_sql_statement(self):
-#         user_repo = UserRepository()
-#         expected_result = "INSERT INTO users (firstName, lastName, email, password) VALUES ('Matt', 'Twomey', 'matt.twomey@infinityworks.com', 'password');"
-#
-#         result = UserRepository.register('Matt', 'Twomey', 'matt.twomey@infinityworks.com', 'password')
-#
-#         self.assertEqual(expected_result, result)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
+class TestUserRepo(unittest.TestCase):
+
+
+     def test_register_sql_statement(self):
+         result = len(hash_password("aaa"))
+         self.assertEqual(93, result)
+
