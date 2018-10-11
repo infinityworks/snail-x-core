d227ae6de31daf07ce918bb66c5a5072e84296b3
James Sheard <32071575+JamesSheard@users.noreply.github.com>
2018-10-11 11:26:24 +0100

    Fixing Tests

--- core/tests/test_db.py
+++ core/tests/test_db.py
@@ -5,6 +5,7 @@ from mock import patch
 # from core.models.user import User
 from core.repositories.user_repository import UserRepository
 from core import app
+import json
 
 
 def not_a_db_hit():
@@ -14,25 +15,23 @@ def not_a_db_hit():
 @patch("core.db.db_func.connect_to_database")
 class TestUserRepo(unittest.TestCase):
 
+    def setUp(self):
+        data = {}
+        data['firstName'] = 'Test Name'
+        data['lastName'] = 'Last Name'
+        data['email'] = 'test@example.com'
+        data['password'] = 'pass123'
+        self.json_data = json.dumps(data)
+
     def test_register_returns_200(self, mock_connect_db):
-        response = app.test_client().post('/register-user', data=dict(
-            firstName="Test Name",
-            lastName="Last Name",
-            email="test@example.com",
-            password="pass123"
-        ))
+        response = app.test_client().post('/register-user', data=self.json_data)
         self.assertEqual(response.status_code, 200)
 
 
 
     @patch.object(UserRepository, 'register')
     def test_insert_db(self, mock_connect_db, mock_user_source):
-        app.test_client().post('/register-user', data=dict(
-            firstName="Test Name",
-            lastName="Last Name",
-            email="test@example.com",
-            password="pass123"
-        ))
+        app.test_client().post('/register-user', data=self.json_data)
 
         mock_connect_db.assert_called_once_with(
             "Test Name",
