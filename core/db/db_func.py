f74c07732415091eb3c29596756ed5e849ac6902
James Sheard <32071575+JamesSheard@users.noreply.github.com>
2018-10-11 10:04:56 +0100

    Update db_func.py

--- core/db/db_func.py
+++ core/db/db_func.py
@@ -25,7 +25,7 @@ def connect_to_database():
         host="ec2-23-21-147-71.compute-1.amazonaws.com",
         user="isfktaipxvnmbp",
         password="d3405d7dede20bc84142a6e336c8b476067decd768ac5ee13ccea55fa065b10c",
-        database = "d67lulaq5muhb8"
+        database = "d67lulaq5muhb8",
         port='5432',
         sslmode='require'
     )
