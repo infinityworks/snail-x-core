fd87c497cc2e8ec04407bccc88c3ed76fddb7bf3
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-10-22 17:47:32 +0100

    update SQL query so that open round is no longer checked using status but by date calculation instead.

--- core/source/round_source.py
+++ core/source/round_source.py
@@ -1,14 +1,19 @@
 from core.db.db_func import get_db
-
+from datetime import datetime
 
 def get_round_id():
     db = get_db()
     cursor = db.cursor()
 
-    query = "select round_id from round where status = 'Open'"
+    current_time = datetime.now()
+    args = (current_time, current_time)
+    query = """SELECT round.round_id 
+            FROM round JOIN race ON round.round_id = race.round_id 
+            WHERE round.start_date <= '{}' 
+            AND race.race_date >= '{}'""".format(current_time, current_time)
 
     try:
-        cursor.execute(query)
+        cursor.execute(query, args)   # inserts the current date and time in to the above SQL query
         db.commit()
     except db.Error as err:
         print(err)
