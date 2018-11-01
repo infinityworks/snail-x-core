f184e2ac827a3a7a61406a0fe8405552567807c1
Emily <emily.birch@infinityworks.com>
2018-11-01 16:32:37 +0000

    FINAL FINAL fixes for homepage HOPEFULLY

--- core/__init__.py
+++ core/__init__.py
@@ -7,7 +7,9 @@ CORS(app)
 from core.routers.result_router import result_router
 from core.routers.round_router import round_router
 from core.routers.user_router import user_router
+from core.routers.prediction_router import prediction_router
 
 app.register_blueprint(user_router)
 app.register_blueprint(round_router)
 app.register_blueprint(result_router)
+app.register_blueprint(prediction_router)
--- core/routers/prediction_router.py
+++ core/routers/prediction_router.py
@@ -33,3 +33,35 @@ def get_predictions():
         return return_data, status.HTTP_200_OK
     else:
         return {"message": "Error. No predictions made"}, status.HTTP_204_NO_CONTENT
+
+
+@prediction_router.route("/get-closed-predictions", methods=["POST"])
+def get_closed_round_predictions():
+    form_data = request.get_json()
+    user_repository = UserRepository()
+    predictions = user_repository.get_predictions_and_results(form_data['userEmail'], form_data['roundID'])
+    if predictions:
+        return predictions
+    else:
+        return {"message": "No predictions made"}, status.HTTP_204_NO_CONTENT
+
+@prediction_router.route("/get-predictions-and-results", methods=["POST"])
+def get_predictions_and_results():
+    form_data = request.get_json()
+    user_repository = UserRepository()
+    predictions = user_repository.get_predictions_and_results(form_data['userEmail'], form_data['roundID'])
+
+    return predictions, status.HTTP_200_OK
+
+
+@prediction_router.route("/specific-user-predictions", methods=["POST"])
+def get_specific_round_predictions():
+    form_data = request.get_json()
+    user_repository = UserRepository()
+    predictions = user_repository.get_specific_round_predictions(form_data['userEmail'], form_data['roundID'])
+
+    if predictions:
+        return_data = json.dumps(predictions)
+        return return_data, status.HTTP_200_OK
+    else:
+        return {"message": "Error. No predictions made"}, status.HTTP_204_NO_CONTENT
