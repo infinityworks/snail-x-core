e88ae80bb3b81d192f94923eed1410e252fc1ce7
JamesSheard <james.sheard@infinityworks.com>
2018-11-01 16:42:49 +0000

    DB Fix

--- core/db/db_func.py
+++ core/db/db_func.py
@@ -20,13 +20,13 @@ def teardown_db(teardown):
         db.close()
 
 
- def connect_to_database():
-     return psycopg2.connect(
-         host="ec2-23-21-147-71.compute-1.amazonaws.com",
-         user="isfktaipxvnmbp",
-         password="d3405d7dede20bc84142a6e336c8b476067decd768ac5ee13ccea55fa065b10c",
-         database="d67lulaq5muhb8"
-     )
+def connect_to_database():
+    return psycopg2.connect(
+        host="ec2-23-21-147-71.compute-1.amazonaws.com",
+        user="isfktaipxvnmbp",
+        password="d3405d7dede20bc84142a6e336c8b476067decd768ac5ee13ccea55fa065b10c",
+        database="d67lulaq5muhb8"
+    )
 
 #def connect_to_database():
 #    return psycopg2.connect(
